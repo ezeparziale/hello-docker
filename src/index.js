bcbd2003bb359024cb9dfc107529883126e25c9b
Ezequiel Parziale <eparziale@gmail.com>
2021-05-30 22:09:37 -0300

    Update con mongodb+ejs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 require('dotenv').config()
 const express = require('express')
 const fileSystem = require('fs');
-const app = express()
+var path = require('path');
+
+// Definicion de utilizacion de express
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 
 //Variables
 const HOST = process.env.HOST;
@@ -14,61 +22,76 @@ const DB_USER = process.env.DB_USER;
 const DB_PASS = process.env.DB_PASS;
 const FILE_PLAYERS = './data/players.json';
 
+var db;
+
+
 console.log(`NODE_ENV=${process.env.NODE_ENV}`);
 
-// app
-app.get('/', (req, res) => {
-  res.send('hello-docker con mongodb')
-})
+
+// Definicion de las rutas de acceso
+var indexRouter = require('./routes/index');
+
+// Permitiendo el acceso a la base de datos mongoDB
+app.use(function(req, res, next){
+  req.db = db;
+  next();
+});
+
+// Definicion de rutas de pagina
+app.use('/', indexRouter);
 
 app.listen(PORT, () => {
   console.log(`App escuchando en: http://${HOST}:${PORT}`)
 })
 
+module.exports = app;
+
 // Mongodb
-var mongoose = require('mongoose');
-mongoose
-  .connect(`mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`, 
-            {
-              auth: {
-                  user: DB_USER,
-                  password: DB_PASS
-              },
-              authSource:"admin",
-              useUnifiedTopology: true,
-              useNewUrlParser: true
-            }
-          )
-  .catch(error => { 
+const { MongoClient } = require("mongodb");
+const { Console } = require('console');
+
+// Connection URL
+const url = `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin&useUnifiedTopology=true&useNewUrlParser=true`;
+const opts = "";
+
+// Create a new MongoClient
+const client = new MongoClient(url);
+
+mongoRun();
+
+async function mongoRun() {
+  try {
+    // Connect the client to the server
+    await client.connect();
+    
+    // Establish and verify connection
+    await client.db(`${DB_NAME}`).command({ ping: 1 });
+
+    db = client.db(`${DB_NAME}`);
+
+    console.log("Connected successfully to server");
+
+    inicializacionColeccion(db, DB_COLLECTION, FILE_PLAYERS);
+
+  } catch (error) {
+    // Ensures that the client will close when you finish/error
     console.log(`Error de conexion con la db: ${error.message}`); 
     process.exit(1)
-  });
-
-inicializarDB();
-
-async function inicializarDB(){
-  const db = mongoose.connection;
-  
-  db.on('error', function(){
-    console.log('+Error de conexion con la db1')
-  });
-  db.once('open', function() {
-    console.log('+Conectados a mongodb')
-    inicializacionColeccion(db, DB_COLLECTION, FILE_PLAYERS);
-  });
+  }
 }
+
 
 async function inicializacionColeccion (db, dbCollectionName, fileName) {
   console.log('+Inicialización de db')
   try {
     // Borrado de colección si existe
     console.log('+Borrado de colección:', dbCollectionName);
-    try {
-      await db.collection(dbCollectionName).drop();
-      console.log('-Se elimino la colección',dbCollectionName)
-    } catch (err) {
-      console.log('-No existe la colección',dbCollectionName)
-    }
+    db.collection(dbCollectionName).drop(function (err, result) {
+      if (err)       
+        console.log('-No existe la colección',dbCollectionName)
+      if (result)
+        console.log('-Se elimino la colección',dbCollectionName);
+  });
 
     // Lectura del archivo json
     console.log('+Leyendo arhivo JSON:', fileName);
@@ -77,10 +100,12 @@ async function inicializacionColeccion (db, dbCollectionName, fileName) {
 
     // Carga de datos a la db
     console.log('+Insertando registros en colección:', dbCollectionName);
-    await db.collection(dbCollectionName).insertMany(jsonDataset);
+    const options = { ordered: true };
+    const result = await db.collection(dbCollectionName).insertMany(jsonDataset, options);
+    console.log(`${result.insertedCount} registros insertados`);
 
     console.log('+Inicializacón finalizada OK');
   } catch (err) {
-    throw err;
+    Console.log(err)
   }
 }
